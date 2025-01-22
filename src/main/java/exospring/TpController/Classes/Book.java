015d1a16486a0741f9a1604fe99c5513af8e14ae
pisha <pisha@hotmail.fr>
2025-01-22 09:25:46 +0100

    [INIT] - first init

--- src/main/java/exospring/TpController/Classes/Book.java
+++ src/main/java/exospring/TpController/Classes/Book.java
@@ -1,4 +1,21 @@
 package exospring.TpController.Classes;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private Date release;
 }
--- src/main/java/exospring/TpController/Classes/Member.java
+++ src/main/java/exospring/TpController/Classes/Member.java
@@ -1,4 +1,17 @@
 package exospring.TpController.Classes;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Member {
+    private int id;
+    private String name;
+    private String email;
 }
